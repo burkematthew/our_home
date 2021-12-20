96af9460906d22b4c6884937469559e59ba5febf
Matthew Burke <burkematthewjoseph@gmail.com>
2021-12-19 20:22:52 -0600

    Saving the member model annotations

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id          :bigint           not null, primary key
+#  birthdate   :date
+#  first_name  :text
+#  last_name   :text
+#  middle_name :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Member < ApplicationRecord
 end
--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id          :bigint           not null, primary key
+#  birthdate   :date
+#  first_name  :text
+#  last_name   :text
+#  middle_name :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "test_helper"
 
 class MemberTest < ActiveSupport::TestCase
