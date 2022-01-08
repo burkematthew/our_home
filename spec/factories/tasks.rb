b1931e5296579c93d86f9a87728a8485e3ecb192
Matthew Burke <burkematthewjoseph@gmail.com>
2022-01-08 17:22:06 -0600

    Adding association between task and list (#42)

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -12,15 +12,18 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  assignable_id   :bigint
+#  list_id         :bigint
 #
 # Indexes
 #
 #  index_tasks_on_assignable  (assignable_type,assignable_id)
 #  index_tasks_on_due_date    (due_date)
+#  index_tasks_on_list_id     (list_id)
 #
 FactoryBot.define do
   factory :task do
     description { "Clean master bathroom" }
     priority { 1 }
+    association :list
   end
 end
