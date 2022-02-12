c82315e3861f645933aff1db0a0e1d66f04ccf03
Matthew Burke <burkematthewjoseph@gmail.com>
2022-02-12 08:53:57 -0600

    [FEATURE] - Upgrade to Rails 7 (#81)

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_09_152541) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_02_10_133861) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "assignees", force: :cascade do |t|
     t.string "assignable_type"
     t.bigint "assignable_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "member_id"
     t.index ["assignable_type", "assignable_id"], name: "index_assignees_on_assignable"
     t.index ["member_id"], name: "index_assignees_on_member_id"
@@ -27,8 +54,8 @@ ActiveRecord::Schema.define(version: 2022_01_09_152541) do
 
   create_table "budget_categories", force: :cascade do |t|
     t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "budgets", force: :cascade do |t|
@@ -36,25 +63,25 @@ ActiveRecord::Schema.define(version: 2022_01_09_152541) do
     t.string "description"
     t.integer "cash_flow_type"
     t.decimal "amount", precision: 10, scale: 2
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "budget_category_id"
     t.index ["budget_category_id"], name: "index_budgets_on_budget_category_id"
   end
 
   create_table "event_types", force: :cascade do |t|
     t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "events", force: :cascade do |t|
     t.text "description"
-    t.datetime "starts_at"
-    t.datetime "ends_at"
+    t.datetime "starts_at", precision: nil
+    t.datetime "ends_at", precision: nil
     t.bigint "event_type_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "assignable_type"
     t.bigint "assignable_id"
     t.index ["assignable_type", "assignable_id"], name: "index_events_on_assignable"
@@ -66,8 +93,8 @@ ActiveRecord::Schema.define(version: 2022_01_09_152541) do
     t.string "description"
     t.string "assignable_type"
     t.bigint "assignable_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["assignable_type", "assignable_id"], name: "index_lists_on_assignable"
   end
 
@@ -76,16 +103,16 @@ ActiveRecord::Schema.define(version: 2022_01_09_152541) do
     t.text "middle_name"
     t.text "last_name"
     t.date "birthdate"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
     t.text "description"
     t.date "due_date"
     t.integer "priority"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "assignable_type"
     t.bigint "assignable_id"
     t.bigint "list_id"
@@ -95,5 +122,7 @@ ActiveRecord::Schema.define(version: 2022_01_09_152541) do
     t.index ["list_id"], name: "index_tasks_on_list_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "events", "event_types"
 end
