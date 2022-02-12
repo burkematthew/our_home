c316cf79b40f0b536e7e31c27d5de4365504d5a1
Matthew Burke <burkematthewjoseph@gmail.com>
2022-02-12 09:07:44 -0600

    Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :members
 
-  root to: 'home#index'
+  root to: "home#index"
 end
